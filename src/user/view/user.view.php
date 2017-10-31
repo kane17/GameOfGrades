129a385b699b2b2d7457754abe7f4d0fb734e228
Remo Reber <remo.reber@iet-gibb.ch>
2017-10-31 11:28:29 +0100

    Angabe für ersten Test im Browser

--- src/user/view/user.view.php
+++ src/user/view/user.view.php
@@ -5,3 +5,12 @@
  * Date: 31.10.2017
  * Time: 10:48
  */
+?>
+<head>
+    <link rel="stylesheet" href="/bootstrap/bootstrap-4.0.0-beta.2-dist/css/bootstrap.css">
+</head>
+<body>
+<h1>Notentool</h1>
+<h4>Benutzer verwalten</h4>
+</body>
+<script src="/bootstrap/bootstrap-4.0.0-beta.2-dist/js/bootstrap.js"></script>
