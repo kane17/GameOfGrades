30b9a91500ec2bffff7c6d709f08a504a3b8aec5
Keijo Nierula <keijo.nierula@iet-gibb.ch>
2017-10-31 10:47:33 +0100

    -restructurized /certificate

--- src/certificate/view/certificate.view.php
+++ src/certificate/view/certificate.view.php
@@ -13,7 +13,13 @@
     <link rel="stylesheet" href="/bootstrap/bootstrap-4.0.0-beta.2-dist/css/bootstrap.css">
 </head>
 <body>
+<h1>Notentool</h1>
+<h4>Zeugnisse verwalten</h4>
+<div class="container">
+    <div class="row">
 
+    </div>
+</div>
 </body>
 <script src="/bootstrap/bootstrap-4.0.0-beta.2-dist/js/bootstrap.js"></script>
 
---
+++ src/model/Certificate.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: vmadmin
+ * Date: 31.10.2017
+ * Time: 10:03
+ */
+
+class Certificate {
+
+    var $id;
+    var $path;
+    var $title;
+    var $userId;
+
+    /**
+     * @return mixed
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param mixed $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+
+}
+
+?>
+
