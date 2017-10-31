6d50e71d98b52cca4316fd51f25ad79bc4f0e594
Keijo Nierula <keijo.nierula@iet-gibb.ch>
2017-10-31 10:21:20 +0100

    - added bootstrap;

--- certificate/certificate.php
+++ certificate/certificate.php
@@ -6,7 +6,10 @@
  * Time: 10:03
  */
 
-
-
+echo 'hello';
 
 ?>
+
+<!-- bootstrap usage -->
+<link rel="stylesheet" href="../bootstrap/bootstrap-4.0.0-beta.2-dist/css/bootstrap.css">
+<script src="../bootstrap/bootstrap-4.0.0-beta.2-dist/js/bootstrap.js"></script>
