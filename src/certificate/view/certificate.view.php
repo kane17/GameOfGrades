30b191a29f9fd48ec7595f213e86f5b63edcd012
Keijo Nierula <keijo.nierula@iet-gibb.ch>
2017-10-31 10:33:06 +0100

    -restructurized /certificate

---
+++ src/certificate/view/certificate.view.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: vmadmin
+ * Date: 31.10.2017
+ * Time: 10:26
+ */
+
+
+?>
+<head>
+    <!-- bootstrap usage -->
+    <link rel="stylesheet" href="/bootstrap/bootstrap-4.0.0-beta.2-dist/css/bootstrap.css">
+</head>
+<body>
+
+</body>
+<script src="/bootstrap/bootstrap-4.0.0-beta.2-dist/js/bootstrap.js"></script>
+
