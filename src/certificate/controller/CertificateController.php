10b9c08d58a7fc7b91498f2e4ba0493eacea892e
Keijo Nierula <keijo.nierula@iet-gibb.ch>
2017-11-07 08:56:41 +0100

    - added style for form; - edited controller; - added google material icons;

--- src/certificate/controller/CertificateController.php
+++ src/certificate/controller/CertificateController.php
@@ -6,7 +6,32 @@
  * Time: 10:26
  */
 
+$certificate = include  "../../model/Certificate.php";
+$user = include "../../model/User.php";
+
 class CertificateController
 {
+
+//    public function __construct(){
+//
+//        $this->console_log($_POST);
+//        $this->console_log($_FILES);
+//
+//    }
+
+    public function submitForm(){
+        $this->console_log($_POST);
+        $this->console_log($_FILES);
+    }
+
+    function console_log( $data ){
+        echo '<script>';
+        echo 'console.log('. json_encode( $data ) .')';
+        echo '</script>';
+    }
+
+
+
+//    $certificates =
 
 }
