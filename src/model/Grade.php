55df88f1b0bc094d3e436946b4d5a03bdc6998bb
Fabian Rutishauser <fabian.rutishauser@iet-gibb.ch>
2017-11-09 19:55:41 +0100

    implementation of db interfaces finished

--- src/model/Grade.php
+++ src/model/Grade.php
@@ -127,7 +127,7 @@ class Grade
     /**
      * @return mixed
      */
-    public function getisCertificateRelevant()
+    public function isCertificateRelevant()
     {
         return $this->isCertificateRelevant;
     }
